2bb76fea8ccffa2deaa74a46a8a60626a81e1445
bilal-rh <bilal.ashraf@live.com>
2024-10-02 01:24:11 +0400

    yy

--- sampleapp/Controllers/HomeController.cs
+++ sampleapp/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using sampleapp.Models;
-using RestSharp
 
-namespace sampleapp.Controllers;
+
 
 public class HomeController : Controller
 {
@@ -18,29 +17,29 @@ public class HomeController : Controller
     {
   
 
-        var options = new RestClientOptions("http://localhost:50547")
-        {
-        MaxTimeout = -1,
-        };
+        // var options = new RestClientOptions("http://localhost:50547")
+        // {
+        // MaxTimeout = -1,
+        // };
 
-        var client = new RestClient(options);
-        var request = new RestRequest("/v1/chat/completions", Method.Post);
-        request.AddHeader("Content-Type", "application/json");
-        var body = @"{" + "\n" +
-        @"  ""messages"": [" + "\n" +
-        @"    {" + "\n" +
-        @"      ""content"": ""You are a helpful assistant.""," + "\n" +
-        @"      ""role"": ""system""" + "\n" +
-        @"    }," + "\n" +
-        @"    {" + "\n" +
-        @"      ""content"": ""What is the capital of France?""," + "\n" +
-        @"      ""role"": ""user""" + "\n" +
-        @"    }" + "\n" +
-        @"  ]" + "\n" +
-        @"}";
-        request.AddStringBody(body, DataFormat.Json);
-        RestResponse response = await client.ExecuteAsync(request);
-        Console.WriteLine(response.Content);
+        // var client = new RestClient(options);
+        // var request = new RestRequest("/v1/chat/completions", Method.Post);
+        // request.AddHeader("Content-Type", "application/json");
+        // var body = @"{" + "\n" +
+        // @"  ""messages"": [" + "\n" +
+        // @"    {" + "\n" +
+        // @"      ""content"": ""You are a helpful assistant.""," + "\n" +
+        // @"      ""role"": ""system""" + "\n" +
+        // @"    }," + "\n" +
+        // @"    {" + "\n" +
+        // @"      ""content"": ""What is the capital of France?""," + "\n" +
+        // @"      ""role"": ""user""" + "\n" +
+        // @"    }" + "\n" +
+        // @"  ]" + "\n" +
+        // @"}";
+        // request.AddStringBody(body, DataFormat.Json);
+        // RestResponse response = await client.ExecuteAsync(request);
+        // Console.WriteLine(response.Content);
 
 
         return View();
